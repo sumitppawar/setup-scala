11f256820c5dbb3c5912f355d73769c4f74a1947
Sumit <sumitppawar@gmail.com>
2020-03-27 16:58:03 +0800

    Removing java installation

--- src/install.ts
+++ src/install.ts
@@ -7,69 +7,11 @@ const bin = path.join(homedir, "bin");
 
 export async function install(javaVersion: string, jabbaVersion: string) {
   setEnvironmentVariableCI();
-  installJava(javaVersion, jabbaVersion);
   installSbt();
 }
 
 function setEnvironmentVariableCI() {
   core.exportVariable("CI", "true");
-}
-
-function jabbaUrlSuffix(): string {
-  const runnerOs = shell.env["RUNNER_OS"] || "undefined";
-  switch (runnerOs.toLowerCase()) {
-    case "linux":
-      return "linux-amd64";
-    case "macos":
-      return "darwin-amd64";
-    case "windows":
-      return "windows-amd64.exe";
-    default:
-      throw new Error(
-        `unknown runner OS: ${runnerOs}, expected one of Linux, macOS or Windows.`
-      );
-  }
-}
-
-function isWindows(): boolean {
-  return shell.env["RUNNER_OS"] === "Windows";
-}
-
-function jabbaName(): string {
-  if (isWindows()) return "jabba.exe";
-  else return "jabba";
-}
-
-function installJava(javaVersion: string, jabbaVersion: string) {
-  core.startGroup("Install Java");
-  core.addPath(bin);
-  const jabbaUrl = `https://github.com/shyiko/jabba/releases/download/${jabbaVersion}/jabba-${jabbaVersion}-${jabbaUrlSuffix()}`;
-  shell.mkdir(bin);
-  const jabba = path.join(bin, jabbaName());
-  shell.set("-ev");
-  shell.exec(`curl -sL -o ${jabba} ${jabbaUrl}`, { silent: true });
-  shell.chmod(755, jabba);
-  const toInstall = shell
-    .exec(`${jabba} ls-remote`)
-    .grep(javaVersion)
-    .head({ "-n": 1 })
-    .stdout.trim();
-  if (!toInstall) {
-    core.setFailed(`Couldn't find Java ${javaVersion}. To fix this problem, run 'jabba ls-remote' to see the list of valid Java versions.`);
-    return;
-  }
-  console.log(`Installing ${toInstall}`);
-  const result = shell.exec(`${jabba} install ${toInstall}`);
-  if (result.code > 0) {
-    core.setFailed(`Failed to install Java ${javaVersion}, Jabba stderr: ${result.stderr}`);
-    return;
-  }
-  const javaHome = shell
-    .exec(`${jabba} which --home ${toInstall}`)
-    .stdout.trim();
-  core.exportVariable("JAVA_HOME", javaHome);
-  core.addPath(path.join(javaHome, "bin"));
-  core.endGroup();
 }
 
 function installSbt() {
